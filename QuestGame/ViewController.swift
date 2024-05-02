231e25ca18e086cfca7490e8cc1649c30d5b44a4
ShAglarov <shamil.aglarov@gmail.com>
2024-05-02 23:38:14 +0300

    Initial Commit

---
+++ QuestGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestGame
+//
+//  Created by Shamil Aglarov on 02.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
